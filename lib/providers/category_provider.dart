02afc4a8ce76364ee50f620d4b1501b1edcb7bfe
otisdoan <ledoanhieu12a6@gmail.com>
2026-06-25 14:11:17 +0700

    feat: replace repository-based category fetching with hardcoded list of categories

--- lib/providers/category_provider.dart
+++ lib/providers/category_provider.dart
@@ -7,6 +7,56 @@ final categoryRepositoryProvider = Provider<CategoryRepository>((ref) {
 });
 
 final categoriesFutureProvider = FutureProvider<List<CategoryModel>>((ref) async {
-  final repository = ref.watch(categoryRepositoryProvider);
-  return repository.getCategories();
+  return const [
+    CategoryModel(
+      id: 'e528a1ba-1b91-4d1a-8e2b-ec1d2e1b1003',
+      name: 'Cơm',
+      imageUrl: 'https://i-giadinh.vnecdn.net/2023/11/11/Bc5Thnhphm15-1699693079-2955-1699693082.jpg',
+    ),
+    CategoryModel(
+      id: 'ec3f1f78-2b21-4a30-80e8-14a374220f13',
+      name: 'Phở',
+      imageUrl: 'https://asrp-image.s3.ap-southeast-1.amazonaws.com/categories/ec3f1f78-2b21-4a30-80e8-14a374220f13/2026/06/77bd7af24dce42e79c4e302a4a2f09e5.jpg',
+    ),
+    CategoryModel(
+      id: 'e528a1ba-1b91-4d1a-8e2b-ec1d2e1b1002',
+      name: 'Bún',
+      imageUrl: 'https://monngonmoingay.com/wp-content/uploads/2018/08/bun-bo-gio-heo-500-min.jpg',
+    ),
+    CategoryModel(
+      id: 'e528a1ba-1b91-4d1a-8e2b-ec1d2e1b1007',
+      name: 'Hủ Tiếu',
+      imageUrl: 'https://cooponline.vn/tin-tuc/wp-content/uploads/2025/10/hu-tieu-nam-vang-cong-thuc-nau-chuan-vi-sai-gon-nuoc-dung-ngot-thanh-dam-da-topping.png',
+    ),
+    CategoryModel(
+      id: 'e528a1ba-1b91-4d1a-8e2b-ec1d2e1b1012',
+      name: 'Ăn Vặt',
+      imageUrl: 'https://asrp-image.s3.ap-southeast-1.amazonaws.com/categories/e528a1ba-1b91-4d1a-8e2b-ec1d2e1b1012/2026/06/be86c1bfc2864284ab8f78d89522dcec.jpg',
+    ),
+    CategoryModel(
+      id: 'b09f7c22-222d-47c9-b11f-d9c9688680c5',
+      name: 'Đồ Uống',
+      imageUrl: 'https://asrp-image.s3.ap-southeast-1.amazonaws.com/categories/b09f7c22-222d-47c9-b11f-d9c9688680c5/2026/06/c5178a8af27b4ea686d827c58fa5715d.jpg',
+    ),
+    CategoryModel(
+      id: 'e528a1ba-1b91-4d1a-8e2b-ec1d2e1b1005',
+      name: 'Tráng miệng',
+      imageUrl: 'https://asrp-image.s3.ap-southeast-1.amazonaws.com/categories/e528a1ba-1b91-4d1a-8e2b-ec1d2e1b1005/2026/06/3e3dd00f20604d1a8cc60b94f38e2006.jpg',
+    ),
+    CategoryModel(
+      id: 'e528a1ba-1b91-4d1a-8e2b-ec1d2e1b1006',
+      name: 'Món Khai Vị',
+      imageUrl: 'https://asrp-image.s3.ap-southeast-1.amazonaws.com/categories/e528a1ba-1b91-4d1a-8e2b-ec1d2e1b1006/2026/06/6745a7aff5914cd8846085c29f9010dc.jpg',
+    ),
+    CategoryModel(
+      id: 'e528a1ba-1b91-4d1a-8e2b-ec1d2e1b1008',
+      name: 'Món Gỏi',
+      imageUrl: 'https://cdn.tgdd.vn/2021/09/CookRecipe/Avatar/goi-buoi-tom-thit-thumbnail.jpg',
+    ),
+    CategoryModel(
+      id: '477bce74-a761-48cc-92d2-b0f9fbb20750',
+      name: 'Phở Xào & Trộn',
+      imageUrl: 'https://asrp-image.s3.ap-southeast-1.amazonaws.com/categories/477bce74-a761-48cc-92d2-b0f9fbb20750/2026/06/d740cc7c8a4b40cebf669f374d26d44a.jpg',
+    ),
+  ];
 });
